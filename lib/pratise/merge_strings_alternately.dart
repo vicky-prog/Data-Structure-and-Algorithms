149b2b2d2b58cbb1e3314015080633861b4d3738
vicky-prog <johnvick281997@gmail.com>
2025-02-01 08:22:36 +0530

    merge string alternately

---
+++ lib/pratise/merge_strings_alternately.dart
@@ -0,0 +1,54 @@
+class Solution {
+  String mergeAlternately(String word1, String word2) {
+    StringBuffer mergedString = StringBuffer();  // Using StringBuffer for efficient string concatenation
+    int i = 0, j = 0;
+
+    // Loop until both strings are fully processed
+    while (i < word1.length || j < word2.length) {
+      if (i < word1.length) {
+        mergedString.write(word1[i]);
+        i++;
+      }
+      if (j < word2.length) {
+        mergedString.write(word2[j]);
+        j++;
+      }
+    }
+
+    return mergedString.toString();  // Convert StringBuffer to a String and return
+  }
+}
+
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+// Example 2:
+
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b 
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+// Example 3:
+
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q 
+// merged: a p b q c   d
+
+
+
